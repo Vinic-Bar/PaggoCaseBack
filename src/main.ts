e19ac264cafda96881a1b36312391fbb52f2bc6c
Vinic-Bar <vinicius.bv160701@gmail.com>
2025-03-18 22:38:38 -0300

    CORS update

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Permite requisições dessas origens
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://paggo-case-front-7jqj.vercel.app'], // Permite requisições dessas origens
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
